acdff36ecb2bf6da39650fbb14c56107c9579e1d
Anil463 <aniltamang462@gmail.com>
2025-06-20 15:11:08 +0545

    bootstrap layout basic format added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/layout')
+@section('header')
+    This is header section
+@endsection
+
+
+@section('content')
+    <h2>Welcome to HRM Project Setup</h2>
+    <p>This is where your main content will go. Add whatever you like here.</p>
+@endsection
+
+
+@section('footer')
+    THis is Footer Section
+@endsection
